80b5f07c0d0b0f5f6b360c2db8dce03952ae9fac
miguel-sobong <migueltsobong@gmail.com>
2024-06-13 23:46:00 +0800

    templating

---
+++ blog_renderer/renderer.go
@@ -0,0 +1,49 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
+)
+
+type PostRenderer struct {
+	templ    *template.Template
+	mdParser *parser.Parser
+}
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	parser := parser.NewWithExtensions(parser.CommonExtensions | parser.AutoHeadingIDs)
+
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostViewModel(p, r))
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
+
+type postViewModel struct {
+	Post
+	HTMLBody template.HTML
+}
+
+func newPostViewModel(p Post, r *PostRenderer) postViewModel {
+	htmlBody := template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return postViewModel{Post: p, HTMLBody: htmlBody}
+}
