0dab250cc2e214582b0db3f875beb56253c898eb
miguel-sobong <migueltsobong@gmail.com>
2024-07-28 21:44:53 +0800

    fix bug when writing shorter to db

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"os"
 	"testing"
 )
@@ -71,7 +70,7 @@ func TestFileSystemStore(t *testing.T) {
 	})
 }
 
-func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+func createTempFile(t testing.TB, initialData string) (*os.File, func()) {
 	t.Helper()
 
 	tmpfile, err := os.CreateTemp("", "db")
