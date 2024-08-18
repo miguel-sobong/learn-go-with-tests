d5027f2de6693b07368d4f7062e49bd99b858676
miguel-sobong <migueltsobong@gmail.com>
2024-08-18 21:12:08 +0800

    purposely error for dependency

--- http-server/CLI_test.go
+++ http-server/CLI_test.go
@@ -1,6 +1,7 @@
 package poker_test
 
 import (
+	"bytes"
 	"fmt"
 	poker "learn-go-with-tests/http-server"
 	"strings"
@@ -9,13 +10,14 @@ import (
 )
 
 var dummySpyAlerter = &poker.SpyBlindAlerter{}
+var dummyStdOut = &bytes.Buffer{}
 
 func TestCLI(t *testing.T) {
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
 		playerStore := &poker.StubPlayerStore{}
 
-		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli := poker.NewCLI(playerStore, in, dummyStdOut, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Chris")
@@ -25,7 +27,7 @@ func TestCLI(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
 		playerStore := &poker.StubPlayerStore{}
 
-		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli := poker.NewCLI(playerStore, in, dummyStdOut, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Cleo")
@@ -36,21 +38,21 @@ func TestCLI(t *testing.T) {
 		playerStore := &poker.StubPlayerStore{}
 		blindAlerter := &poker.SpyBlindAlerter{}
 
-		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli := poker.NewCLI(playerStore, in, dummyStdOut, blindAlerter)
 		cli.PlayPoker()
 
 		cases := []poker.ScheduledAlert{
-		{0 * time.Second, 100},
-		{10 * time.Minute, 200},
-		{20 * time.Minute, 300},
-		{30 * time.Minute, 400},
-		{40 * time.Minute, 500},
-		{50 * time.Minute, 600},
-		{60 * time.Minute, 800},
-		{70 * time.Minute, 1000},
-		{80 * time.Minute, 2000},
-		{90 * time.Minute, 4000},
-		{100 * time.Minute, 8000},
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
 		}
 
 		for i, want := range cases {
@@ -63,5 +65,43 @@ func TestCLI(t *testing.T) {
 				poker.AssertScheduledAlert(t, got, want)
 			})
 		}
+	})
+
+	var dummyPlayerStore = &poker.StubPlayerStore{}
+
+	t.Run("im prompts the user to enter the number of players", func(t *testing.T) {
+		stdout := &bytes.Buffer{}
+		in := strings.NewReader("7\n")
+		blindAlerter := &poker.SpyBlindAlerter{}
+
+		cli := poker.NewCLI(dummyPlayerStore, in, stdout, blindAlerter)
+		cli.PlayPoker()
+
+		got := stdout.String()
+		want := poker.PlayerPrompt
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+		cases := []poker.ScheduledAlert{
+			{0 * time.Second, 100},
+			{12 * time.Minute, 200},
+			{24 * time.Minute, 300},
+			{36 * time.Minute, 400},
+		}
+
+		for i, want := range cases {
+			t.Run(fmt.Sprint(want), func(t *testing.T) {
+
+				if len(blindAlerter.Alerts) <= i {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.Alerts)
+				}
+
+				got := blindAlerter.Alerts[i]
+				poker.AssertScheduledAlert(t, got, want)
+			})
+		}
+
 	})
 }
